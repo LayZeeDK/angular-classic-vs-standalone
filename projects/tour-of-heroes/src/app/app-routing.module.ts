607f73df6ba3eccb3c0a81f514f9692a5137bf38
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-08-25 12:16:22 +0200

    refactor: extract SCAMs

--- projects/tour-of-heroes/src/app/app-routing.module.ts
+++ projects/tour-of-heroes/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesModule } from './heroes.module';
 
 import { DashboardComponent } from './dashboard.component';
+import { DashboardModule } from './dashboard.module';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroDetailModule } from './hero-detail.module';
 import { HeroesComponent } from './heroes.component';
 
 const routes: Routes = [
@@ -13,7 +16,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    DashboardModule,
+    HeroDetailModule,
+    HeroesModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- projects/tour-of-heroes/src/app/app.module.ts
+++ projects/tour-of-heroes/src/app/app.module.ts
@@ -1,31 +1,19 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroSearchComponent } from './hero-search.component';
-import { HeroesComponent } from './heroes.component';
 import { InMemoryDataService } from './in-memory-data.service';
-import { MessagesComponent } from './messages.component';
+import { MessagesModule } from './messages.module';
 
 @NgModule({
   bootstrap: [AppComponent],
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    HeroSearchComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FormsModule,
+    MessagesModule,
     AppRoutingModule,
     HttpClientModule,
 
